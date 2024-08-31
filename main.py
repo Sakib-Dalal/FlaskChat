f0f81b2ca812a785e1d18c4329aa10b4718d301a
Ubuntu <ubuntu@ip-172-31-1-76.ap-south-1.compute.internal>
2024-08-31 06:25:44 +0000

    create files

---
+++ main.py
@@ -0,0 +1,96 @@
+from flask import Flask, request, render_template, redirect, url_for, session
+from flask_socketio import SocketIO, join_room, leave_room, send
+import random
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "supersecretkey"
+socketio = SocketIO(app)
+
+ascii_letters = "93uhf4ibq3idbn3ubdw3iubdlamdbghrbv3oejfwnefqaldwi3urwu"
+rooms = {}
+
+def generate_room_code(length: int, existing_codes: list[str]) -> str:
+    while True:
+        code_chars = [random.choice(ascii_letters) for _ in range(length)]
+        code = ''.join(code_chars)
+        if code not in existing_codes:
+            return code
+
+@app.route('/', methods=["GET", "POST"])
+def home():
+    session.clear()
+    if request.method == "POST":
+        name = request.form.get('name')
+        create = request.form.get('create', False)
+        code = request.form.get('code')
+        join = request.form.get('join', False)
+        if not name:
+            return render_template('home.html', error="Name is required", code=code)
+        if create != False:
+            room_code = generate_room_code(6, list(rooms.keys()))
+            new_room = {
+                'members': 0,
+                'messages': []
+            }
+            rooms[room_code] = new_room
+        if join != False:
+            if not code:
+                return render_template('home.html', error="Please enter a room code to enter a chat room", name=name)
+            if code not in rooms:
+                return render_template('home.html', error="Room code invalid", name=name)
+            room_code = code
+        session['room'] = room_code
+        session['name'] = name
+        return redirect(url_for('room'))
+    else:
+        return render_template('home.html')
+    
+@app.route('/room')
+def room():
+    room = session.get('room')
+    name = session.get('name')
+    if name is None or room is None or room not in rooms:
+        return redirect(url_for('home'))
+    messages = rooms[room]['messages']
+    return render_template('room.html', room=room, user=name, messages=messages)
+
+@socketio.on('connect')
+def handle_connect():
+    name = session.get('name')
+    room = session.get('room')
+    if name is None or room is None:
+        return
+    if room not in rooms:
+        leave_room(room)
+    join_room(room)
+    send({
+        "sender": "",
+        "message": f"{name} has entered the chat"
+    }, to=room)
+    rooms[room]["members"] += 1
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    room = session.get("room")
+    name = session.get("name")
+    leave_room(room)
+    if room in rooms:
+        rooms[room]["members"] -= 1
+        if rooms[room]["members"] <= 0:
+            del rooms[room]
+        send({
+        "message": f"{name} has left the chat",
+        "sender": ""
+    }, to=room)
+
+@socketio.on('message')
+def handle_message(message):
+    room = session.get('room')
+    name = session.get('name')
+    if room is None or name is None:
+        return
+    rooms[room]['messages'].append({'message': message['message'], 'sender': name})
+    send(message, to=room)
+
+if __name__ == "__main__":
+    socketio.run(app, debug=True, host="0.0.0.0", port=80)
